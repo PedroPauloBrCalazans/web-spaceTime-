ad8eab57079aed3252b7212890e826d6169250a6
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2023-06-08 16:01:25 -0300

    feat: Definindo layout das rotas + rota de logout.

---
+++ src/app/memories/new/page.tsx
@@ -0,0 +1,5 @@
+export default function NewMemories() {
+  return(
+    <h1>oi</h1>
+  )
+}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:7474',
+  baseURL: 'http://192.168.1.21:7474',
 })
